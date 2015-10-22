7d63f39758416edfcad7ceeea05f9ba93193c2c2
Mahak-Patil <MahakPatil@dhcp127.merusouth122.iit.edu>
2015-10-22 13:38:44 -0500

    added more to php lines

--- install-env.sh
+++ install-env.sh
@@ -4,6 +4,8 @@ sudo apt-get install apache2 git mysql-client php5
 
 curl -sS https://getcomposer.org/installer | php
 php composer.phar require aws/aws-sdk-php
+<?php
+require 'vendor/autoload.php';
 
 echo  "$1" > /var/www/html/hello.txt
 
