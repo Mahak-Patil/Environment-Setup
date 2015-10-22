2b4d30d4276becf820dfb95f8db19113287360ac
Mahak-Patil <MahakPatil@dhcp127.merusouth122.iit.edu>
2015-10-22 13:37:52 -0500

    modified install-webserver.sh tp get all packages

--- install-env.sh
+++ install-env.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install apache2
-sudo apt-get install git
+sudo apt-get install apache2 git mysql-client php5 
+
+curl -sS https://getcomposer.org/installer | php
+php composer.phar require aws/aws-sdk-php
 
 echo  "$1" > /var/www/html/hello.txt
 
